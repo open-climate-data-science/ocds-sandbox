8151ef1e53ad3fef4e2bab1c2d4a078ad529048b
Yuhan (Douglas) Rao <38257268+geo-yrao@users.noreply.github.com>
2022-02-25 00:48:54 -0500

    Set up env with required R libraries

---
+++ install.R
@@ -0,0 +1,9 @@
+## This file install required R packages for the tutorials when the repo is first binderized
+install.packages("tidyverse")
+install.packages("rmarkdown")        
+install.packages("ggplot2")
+install.packages("lubridate")
+install.packages("readxl")
+install.packages("viridis")
+install.packages("ggridges")
+install.packages("hrbrthemes")
